0c451acd064ce1abba9ffe1c18a00f96b1d0a90c
Adam Traub <adamTraubsEmail@gmail.com>
2019-12-03 21:19:04 -0500

    testing average strategy

--- healthTests.py
+++ healthTests.py
@@ -13,43 +13,48 @@ def main(PINGINTERVAL=1, RUNTIME=1):
         "1",
     )
     
-    RUNTIME = (RUNTIME*60)//PINGINTERVAL
+    RUNTIME = (RUNTIME*10)//PINGINTERVAL
+    fullResults = [list() for i in range(len(ChaosIntervals))]
 
-    for interval in ChaosIntervals:
-        results = []
-        runCube = interval != "baseline"
-        if runCube:
-            startCube = f"docker run --rm --name=chaoskube -v /home/jzheadley/.kube/config:/kube/config quay.io/linki/chaoskube:latest  --kubeconfig=/kube/config --namespaces=default --no-dry-run --interval={interval}s"
-            p = subprocess.Popen(startCube.split())
-            outMessage = f"Pinging every {PINGINTERVAL} second(s) for {RUNTIME*PINGINTERVAL//60} minute(s) with chaos interval of {interval}s"
-        else:
-            outMessage = f"Pinging every {PINGINTERVAL} second(s) for {RUNTIME*PINGINTERVAL//60} minute(s)"
+    for test in range(2):
+        for interval in ChaosIntervals:
+            results = []
+            fullResults.append(results)
+            runCube = interval != "baseline"
+            if runCube:
+                startCube = f"docker run --rm --name=chaoskube -v /home/jzheadley/.kube/config:/kube/config quay.io/linki/chaoskube:latest  --kubeconfig=/kube/config --namespaces=default --no-dry-run --interval={interval}s"
+                p = subprocess.Popen(startCube.split())
+                outMessage = f"Pinging every {PINGINTERVAL} second(s) for {RUNTIME*PINGINTERVAL//60} minute(s) with chaos interval of {interval}s"
+            else:
+                outMessage = f"Pinging every {PINGINTERVAL} second(s) for {RUNTIME*PINGINTERVAL//60} minute(s)"
 
-        print(outMessage)
+            print(outMessage)
 
-        for i in range(RUNTIME):
-            print(f"Ping {i+1} of {RUNTIME}")
-            try:
-                results.append(subprocess.check_output("curl -w '%{time_total}' -so /dev/null http://localhost:8080",shell=True).decode("utf-8")+"\n")
-            except Exception as e:
-                print("ERROR! "+str(e))
-                results.append(str(e)+"\n")
-            finally:
-                sleep(PINGINTERVAL)
+            for i in range(RUNTIME):
+                print(f"Ping {i+1} of {RUNTIME}")
+                try:
+                    results.append(subprocess.check_output("curl -w '%{time_total}' -so /dev/null http://localhost:8080",shell=True).decode("utf-8")+"\n")
+                except Exception as e:
+                    print("ERROR! "+str(e))
+                    results.append(str(e)+"\n")
+                finally:
+                    sleep(PINGINTERVAL)
 
-        if runCube:
-            os.system("docker kill chaoskube")
-            outMessage = f"Ping Test for interval {interval} Done!"
-        else:
-            outMessage = f"Ping Test for baseline Done!"
+            if runCube:
+                os.system("docker kill chaoskube")
+                outMessage = f"Ping Test for interval {interval} Done!"
+            else:
+                outMessage = f"Ping Test for baseline Done!"
 
-        print(outMessage)
+            print(outMessage)
 
-        fName = f"{(int(time()))}-{interval}.txt"
+    for i,v in ChaosIntervals:
+        fName = f"{(int(time()))}-{v}.txt"
         with open(fName,"w") as outFile:
-            outFile.writelines(results)
-        print("File Written!")
-            
+            for row in zip(*fullResults[i]):
+                outFile.write(f"{(sum(row)/len(row)):.6f}\n")
+        print(f"{fName} Written!")
+
     print("Work complete!")
             
 if __name__ == "__main__":
